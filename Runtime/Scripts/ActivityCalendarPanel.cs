4d9bbe750cac970c1e08e3ac4fde72cb109a3b2e
鸭鸭 <qazwsx110as@163.com>
2025-02-20 16:11:42 +0800

    初次提交

---
+++ Runtime/Scripts/ActivityCalendarPanel.cs
@@ -0,0 +1,220 @@
+﻿using UdonSharp;
+using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using VRC.SDK3.Components;
+using VRC.SDK3.Data;
+using VRC.SDK3.Image;
+using VRC.SDK3.StringLoading;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+public class ActivityCalendarPanel : UdonSharpBehaviour
+{
+    [Header("图片轮换时间间隔")]
+    [SerializeField] private float LoadDuration = 5f;
+    [Header("活动信息URL")]
+    [SerializeField] private VRCUrl url;
+    [Header("活动图片URL")]
+    [SerializeField] private VRCUrl imageurl;
+    [SerializeField] private Renderer materialRend;
+    [SerializeField] private RectTransform ActivityContent;
+    [SerializeField] private Transform TagListContent = null;
+    [SerializeField] private Text timenow;
+    [SerializeField] private Text Inform;
+    [SerializeField] private ScrollRect scollview;
+    [SerializeField] private float offsetY;
+    [SerializeField] private Toggle[] PageButton;
+    [SerializeField] private RawImage[] rawImages;
+    private float nextLoadTime = 0f;
+    private Texture2D downloadedTexture;
+    private VRCImageDownloader imageDownloader;
+    private IUdonEventReceiver udonEventReceiver;
+    private float itemhight;
+    [HideInInspector] public DataList _activitys;
+    DataList _taglist;
+    DataDictionary _json;
+    void Start()
+    {
+        itemhight = ActivityContent.GetChild(0).GetComponent<RectTransform>().rect.height;
+        materialRend = GetComponent<Renderer>();
+        downloadedTexture = new Texture2D(2040, 1020);
+        imageDownloader = new VRCImageDownloader();
+        udonEventReceiver = (IUdonEventReceiver)this;
+        timenow.text = DateTime.Now.ToString("yyyy-MM-dd");
+        SendCustomEventDelayedSeconds(nameof(Download), 5);
+        nextLoadTime = Time.time;
+    }
+    public void Download()
+    {
+        var texInfo = new TextureInfo();
+        texInfo.GenerateMipMaps = true;
+        imageDownloader.DownloadImage(imageurl, materialRend.material, udonEventReceiver, texInfo);
+        VRCStringDownloader.LoadUrl(url, udonEventReceiver);
+    }
+    void Update()
+    {
+        if (Time.time - nextLoadTime <= LoadDuration) return;
+        float x = scollview.normalizedPosition.x;
+        float increment = .125f;
+        x += increment;
+        if (x > 1f)
+        {
+            x = 0f; // 归零
+        }
+        scollview.normalizedPosition = new Vector2(x, 0);
+        ChangeButtonColor();
+        nextLoadTime = Time.time;
+    }
+    public void UpdateContentheight()
+    {
+        Debug.Log("_activitys.Count" + _activitys.Count);
+        ActivityContent.sizeDelta = new Vector2(ActivityContent.sizeDelta.x, _activitys.Count * (offsetY + itemhight) + offsetY);
+        Debug.Log("ActivityContent.sizeDelta" + ActivityContent.sizeDelta.y);
+    }
+    public void ChangeButtonColor()
+    {
+        float value = scollview.normalizedPosition.x;
+        for (int i = 0; i < PageButton.Length; i++)
+        {
+            if (value >= (float)i / PageButton.Length && value <= ((float)(i + 1) / PageButton.Length) || value < 0f && i == 0 || Mathf.FloorToInt(value) == 1 && i == PageButton.Length - 1)
+            {
+                PageButton[i].isOn = true;
+            }
+        }
+    }
+    public void GenerateTagList()
+    {
+        Debug.Log("在读取Tag列表");
+        TagListContent.GetChild(0).gameObject.SetActive(true);
+        for (int i = 0; i < TagListContent.childCount; i++)
+        {
+
+            if (_taglist.TryGetValue(i, out var v))
+            {
+                Debug.Log($"[<color=#ff70ab>TagList</color>] 读取Tag列表成功 索引{i}");
+                Text tagText = TagListContent.GetChild(i + 1).Find("Label").GetComponentInChildren<Text>();
+                tagText.text = v.String;
+                TagListContent.GetChild(i + 1).gameObject.SetActive(true);
+            }
+            else
+            {
+                Debug.Log($"[<color=#ff70ab>TagList</color>] 读取Tag列表失败 索引{i}");
+                if (i >= TagListContent.childCount - 1) break;
+                TagListContent.GetChild(i + 1).gameObject.SetActive(false);
+            }
+        }
+    }
+    public override void OnStringLoadSuccess(IVRCStringDownload result)
+    {
+        string ApiBase = result.Result;
+        Debug.Log($"[<color=#ff70ab>StringLoad</color>] 下载字符串成功 内容为 " + ApiBase);
+        if (!VRCJson.TryDeserializeFromJson(result.Result, out var json))
+        {
+            Debug.Log($"[<color=#ff70ab>StringtoJson</color>] 解析json字符串失败. {json.ToString()}");
+            return;
+        }
+        Debug.Log($"json" + json.TokenType);
+        if (json.TokenType != TokenType.DataDictionary) return;
+        _json = json.DataDictionary;
+        if (_json.TryGetValue("Activity", out var ActivityValue))
+        {
+            Debug.Log($"Activityalue" + ActivityValue.TokenType);
+            _activitys = ActivityValue.DataList;
+        }
+        if (_json.TryGetValue("TagList", out var TagListValue))
+        {
+            Debug.Log($"TagListValue" + TagListValue.TokenType);
+            _taglist = TagListValue.DataList;
+        }
+        if (_json.TryGetValue("inform", out var informValue))
+        {
+            if (Inform != null)
+            {
+                Inform.text = informValue.String;
+            }
+        }
+        if (TagListContent != null)
+        {
+            GenerateTagList();
+        }
+        ItemRefresh();
+        UpdateContentheight();
+        ActivityContentValueChange();
+    }
+    public override void OnStringLoadError(IVRCStringDownload result)
+    {
+        Debug.Log($"[<color=#ff70ab>StringLoad</color>] 下载字符串失败. ");
+    }
+    public override void OnImageLoadSuccess(IVRCImageDownload result)
+    {
+        downloadedTexture = result.Result;
+        foreach (var image in rawImages)
+        {
+            image.texture = downloadedTexture;
+        }
+    }
+    public override void OnImageLoadError(IVRCImageDownload result)
+    {
+        Debug.Log($"Image not loaded: {result.Error.ToString()}: {result.ErrorMessage}.");
+    }
+    public void Refresh()
+    {
+        var texInfo = new TextureInfo();
+        texInfo.GenerateMipMaps = true;
+        imageDownloader.DownloadImage(imageurl, materialRend.material, udonEventReceiver, texInfo);
+        VRCStringDownloader.LoadUrl(url, udonEventReceiver);
+    }
+    public void SearchTag(string tag)
+    {
+        Debug.Log(tag + "li被选中");
+        if (tag == "全部") tag = "";
+        if (_json.TryGetValue("Activity" + tag, out var ActivityValue))
+        {
+            _activitys = ActivityValue.DataList;
+            Debug.Log($"Activityalue" + tag + ActivityValue.TokenType + $"ActivityalueCount" + _activitys.Count);
+        }
+
+        ItemRefresh();
+        UpdateContentheight();
+        ActivityContentValueChange();
+
+    }
+    public void ItemRefresh()
+    {
+        for (int i = 0; i < ActivityContent.childCount; i++)
+        {
+            ActivityContent.GetChild(i).GetComponent<ActivityItem>().OnIdChanged();
+            Debug.Log(ActivityContent.GetChild(i).GetComponent<ActivityItem>().Id);
+        }
+    }
+    public void ActivityContentValueChange()
+    {
+        var positionY = ActivityContent.anchoredPosition.y;
+        positionY = positionY < 0 ? 0 : positionY;
+        int remainder = Mathf.FloorToInt(positionY / (itemhight + offsetY)) % 6;
+        remainder = remainder < 0 ? 0 : remainder;
+        ActivityItemIdChange(Mathf.FloorToInt(positionY / (itemhight + offsetY)), remainder);
+    }
+    public void ActivityItemIdChange(int id, int start_i)
+    {
+        for (int i = start_i; i < ActivityContent.childCount; i++)
+        {
+            ActivityContent.GetChild(i).GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -id * (offsetY + itemhight));
+            Debug.Log(i + "个item的高度为" + ActivityContent.GetChild(i).GetComponent<RectTransform>().anchoredPosition.y);
+            ActivityContent.GetChild(i).GetComponent<ActivityItem>().Id = id;
+            id++;
+        }
+        if (start_i != 0)
+        {
+            for (int i = 0; i < start_i; i++)
+            {
+                ActivityContent.GetChild(i).GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -id * (offsetY + itemhight));
+                Debug.Log(i + "个item的高度为" + ActivityContent.GetChild(i).GetComponent<RectTransform>().anchoredPosition.y);
+                ActivityContent.GetChild(i).GetComponent<ActivityItem>().Id = id;
+                id++;
+            }
+        }
+    }
+}
---
+++ Runtime/Scripts/ActivityItem.cs
@@ -0,0 +1,302 @@
+﻿using UdonSharp;
+using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using VRC.SDK3.Components;
+using VRC.SDK3.Data;
+using VRC.SDK3.Image;
+using VRC.SDK3.StringLoading;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+public class ActivityItem : UdonSharpBehaviour
+{
+    [SerializeField] private ActivityItem ActivitydetailItem;
+    [SerializeField] private ActivityCalendarPanel activitycalendar;
+    [SerializeField] private GameObject Recent;
+    [SerializeField] private GameObject Longterm;
+    private int _id = -1;
+    // 定义 Id 属性
+    public int Id
+    {
+        get { return _id; }
+        set
+        {
+            if (_id != value)
+            {
+                _id = value;
+                OnIdChanged();
+            }
+        }
+    }
+    // 定义 Id 属性
+    public Text[] year;
+    public Text[] date;
+    public Text[] time;
+    public Text[] week;
+    public Text[] starttime;
+    public Text[] endtime;
+    public Text[] title;
+    public Text[] brief;
+    public Text[] startdate;
+    public Text[] enddate;
+    public Text[] startyear;
+    public Text[] endyear;
+    public Text[] initiator;
+    public Text[] join;
+    public Text[] detail;
+    public InputField[] joincopy;
+    void Start()
+    {
+        year = FindTextComponentsInChildren("Year");
+        date = FindTextComponentsInChildren("Date");
+        time = FindTextComponentsInChildren("Time");
+        week = FindTextComponentsInChildren("Week");
+        starttime = FindTextComponentsInChildren("StartTime");
+        endtime = FindTextComponentsInChildren("EndTime");
+        title = FindTextComponentsInChildren("Title");
+        brief = FindTextComponentsInChildren("Brief");
+        startdate = FindTextComponentsInChildren("StartDate");
+        enddate = FindTextComponentsInChildren("EndDate");
+        startyear = FindTextComponentsInChildren("StartYear");
+        endyear = FindTextComponentsInChildren("EndYear");
+        initiator = FindTextComponentsInChildren("Initiator");
+        join = FindTextComponentsInChildren("Join");
+        detail = FindTextComponentsInChildren("Detail");
+        joincopy = FindInputFieldComponentsInChildren("JoinCopy");
+        gameObject.SetActive(false);
+    }
+    private Text[] FindTextComponentsInChildren(string name)
+    {
+        Text[] foundTextComponents = new Text[0];
+        foreach (Transform child in transform)
+        {
+            if (child.name == name)
+            {
+                Text textComponent = child.GetComponent<Text>();
+                if (textComponent != null)
+                {
+                    Text[] newArray = new Text[foundTextComponents.Length + 1];
+                    for (int i = 0; i < foundTextComponents.Length; i++)
+                    {
+                        newArray[i] = foundTextComponents[i];
+                    }
+                    newArray[foundTextComponents.Length] = textComponent;
+                    foundTextComponents = newArray;
+                }
+            }
+            else
+            {
+                Text[] childTextComponents = child.GetComponentsInChildren<Text>(true); // 包括未启用的物体
+                foreach (Text textComponent in childTextComponents)
+                {
+                    if (textComponent.name == name)
+                    {
+                        Text[] newArray = new Text[foundTextComponents.Length + 1];
+                        for (int i = 0; i < foundTextComponents.Length; i++)
+                        {
+                            newArray[i] = foundTextComponents[i];
+                        }
+                        newArray[foundTextComponents.Length] = textComponent;
+                        foundTextComponents = newArray;
+                    }
+                }
+            }
+        }
+        return foundTextComponents;
+    }
+    private InputField[] FindInputFieldComponentsInChildren(string name)
+    {
+        InputField[] foundInputFieldComponents = new InputField[0];
+        foreach (Transform child in transform)
+        {
+            if (child.name == name)
+            {
+                InputField inputFieldComponent = child.GetComponent<InputField>();
+                if (inputFieldComponent != null)
+                {
+                    InputField[] newArray = new InputField[foundInputFieldComponents.Length + 1];
+                    for (int i = 0; i < foundInputFieldComponents.Length; i++)
+                    {
+                        newArray[i] = foundInputFieldComponents[i];
+                    }
+                    newArray[foundInputFieldComponents.Length] = inputFieldComponent;
+                    foundInputFieldComponents = newArray;
+                }
+            }
+            else
+            {
+                InputField[] childInputFieldComponents = child.GetComponentsInChildren<InputField>(true); // 包括未启用的物体
+                foreach (InputField inputFieldComponent in childInputFieldComponents)
+                {
+                    if (inputFieldComponent.name == name)
+                    {
+                        InputField[] newArray = new InputField[foundInputFieldComponents.Length + 1];
+                        for (int i = 0; i < foundInputFieldComponents.Length; i++)
+                        {
+                            newArray[i] = foundInputFieldComponents[i];
+                        }
+                        newArray[foundInputFieldComponents.Length] = inputFieldComponent;
+                        foundInputFieldComponents = newArray;
+                    }
+                }
+            }
+        }
+        return foundInputFieldComponents;
+    }
+    public void OnIdChanged()
+    {
+        Debug.Log("OnIdChanged被调用");
+        if (activitycalendar._activitys.TryGetValue(Id, TokenType.DataDictionary, out var v))
+        {
+            if (v.DataDictionary.TryGetValue("year", TokenType.String, out var yearvalue) && year != null)
+            {
+                foreach (Text textComponent in year)
+                {
+                    textComponent.text = yearvalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("date", TokenType.String, out var datevalue) && date != null)
+            {
+                foreach (Text textComponent in date)
+                {
+                    textComponent.text = datevalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("time", TokenType.String, out var timevalue) && time != null)
+            {
+                foreach (Text textComponent in time)
+                {
+                    textComponent.text = timevalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("week", TokenType.String, out var weekvalue) && week != null)
+            {
+                foreach (Text textComponent in week)
+                {
+                    textComponent.text = weekvalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("startdate", TokenType.String, out var startdatevalue) && startdate != null)
+            {
+                foreach (Text textComponent in startdate)
+                {
+                    textComponent.text = startdatevalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("starttime", TokenType.String, out var starttimevalue) && starttime != null)
+            {
+                foreach (Text textComponent in starttime)
+                {
+                    textComponent.text = starttimevalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("enddate", TokenType.String, out var enddatevalue) && enddate != null)
+            {
+                foreach (Text textComponent in enddate)
+                {
+                    textComponent.text = enddatevalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("endtime", TokenType.String, out var endtimevalue) && endtime != null)
+            {
+                foreach (Text textComponent in endtime)
+                {
+                    textComponent.text = endtimevalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("title", TokenType.String, out var titlevalue) && title != null)
+            {
+                foreach (Text textComponent in title)
+                {
+                    textComponent.text = titlevalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("brief", TokenType.String, out var briefvalue) && brief != null)
+            {
+                foreach (Text textComponent in brief)
+                {
+                    textComponent.text = briefvalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("join", TokenType.String, out var joincopyvalue) && join != null && joincopy != null)
+            {
+                foreach (Text textComponent in join)
+                {
+                    textComponent.text = joincopyvalue.String;
+                }
+                foreach (InputField textComponent in joincopy)
+                {
+                    textComponent.text = joincopyvalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("startyear", TokenType.String, out var startyearvalue) && startyear != null)
+            {
+                foreach (Text textComponent in startyear)
+                {
+                    textComponent.text = startyearvalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("endyear", TokenType.String, out var endyearvalue) && endyear != null)
+            {
+                foreach (Text textComponent in endyear)
+                {
+                    textComponent.text = endyearvalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("initiator", TokenType.String, out var initiatorvalue) && initiator != null)
+            {
+                foreach (Text textComponent in initiator)
+                {
+                    textComponent.text = initiatorvalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("detail", TokenType.String, out var deatailvalue) && detail != null)
+            {
+                foreach (Text textComponent in detail)
+                {
+                    textComponent.text = deatailvalue.String;
+                }
+            }
+            if (v.DataDictionary.TryGetValue("type", TokenType.String, out var typevalue))
+            {
+                if (Recent != null && Longterm != null)
+                {
+                    if (typevalue.String == "0")
+                    {
+                        Recent.SetActive(true);
+                        Longterm.SetActive(false);
+                    }
+                    else
+                    {
+                        Recent.SetActive(false);
+                        Longterm.SetActive(true);
+                    }
+                }
+            }
+            Debug.Log(Id + "有数据开启item");
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            Debug.Log(Id + "无数据关闭item");
+            gameObject.SetActive(false);
+        }
+    }
+    public void ShowActivityDetail()
+    {
+        ActivitydetailItem.Id = Id;
+    }
+    public void SearchTag()
+    {
+        Toggle tagToggle = GetComponent<Toggle>();
+        Text tag = GetComponentInChildren<Text>();
+        if (tagToggle.isOn)
+        {
+            // 在这里编写 Toggle 被选中时触发的操作
+            Debug.Log(tag.text + "被选中");
+            activitycalendar.SearchTag(tag.text);
+        }
+    }
+}
